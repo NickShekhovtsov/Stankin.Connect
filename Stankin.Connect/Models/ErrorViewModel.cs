c27cb0b63a33ddbad7de7a7fbd71255587b552b0
Nikolay Shekhovsov <killerkol@mail.ru>
2021-04-23 19:34:51 +0300

    Добавьте файлы проекта.

---
+++ Stankin.Connect/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stankin.Connect.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
